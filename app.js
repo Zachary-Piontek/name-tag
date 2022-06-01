8061f7d102f761dc24218b12948f3c3e5fdd723d
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-05-31 18:20:50 -0700

    validated input by console log

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // grab DOM elements
 
+const inputName = document.getElementById('input-name');
+const nameDisplay = document.getElementById('name-display');
+
+console.log(inputName)
+
+inputName.addEventListener('input', () => {
+});
 // set event listeners
     // get info from user input
     // use user input to update state
